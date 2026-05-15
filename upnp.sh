8437762d315536eb5d9eefde8eaf3951bda0bb09
ahhfzwl <ahhfzwl@qq.com>
2026-05-15 18:25:58 +0800

    Update upnp.sh

--- upnp.sh
+++ upnp.sh
@@ -8,10 +8,10 @@ curl -s -X POST "http://192.168.1.1:52869/upnp/control/WANIPConn1" \
   <s:Body>
     <u:AddPortMapping xmlns:u=\"urn:schemas-upnp-org:service:WANIPConnection:1\">
       <NewRemoteHost></NewRemoteHost>
-      <NewProtocol>$(echo "$protocol" | tr 'a-z' 'A-Z')</NewProtocol>
       <NewInternalClient>${private_ip}</NewInternalClient>
       <NewInternalPort>${private_port}</NewInternalPort>
       <NewExternalPort>${b}</NewExternalPort>
+      <NewProtocol>$(echo "$protocol" | tr 'a-z' 'A-Z')</NewProtocol>
       <NewEnabled>1</NewEnabled>
       <NewPortMappingDescription>Natter-UPnP</NewPortMappingDescription>
       <NewLeaseDuration>0</NewLeaseDuration>
